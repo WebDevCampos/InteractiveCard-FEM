10dc68a3c776eaae863a33d9fe1304636c2b7773
Marcus Rangel <webdevcmpos@gmail.com>
2022-11-15 11:21:20 -0300

    Added Js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const cardDataForm = document.querySelector(".form-data");
 const completeForm = document.querySelector(".form-complete");
+let cardHolderName = document.querySelector(".cardholder-name");
+let cardFrontName = document.querySelector(".person-name");
+let cardNumber = document.querySelector(".cardholder-number");
+let cardFrontNumber = document.querySelector(".card-front-number");
 cardDataForm.addEventListener("submit", (e) => {
   e.preventDefault();
   completeForm.classList.toggle("show");
@@ -10,4 +14,18 @@ completeForm.addEventListener("submit", (e) => {
   e.preventDefault();
   completeForm.classList.remove("show");
   cardDataForm.classList.remove("hide");
+});
+
+cardDataForm.addEventListener("input", (e) => {
+  if (e.target == cardHolderName) {
+    cardFrontName.textContent = e.target.value.toUpperCase();
+  } else if (e.target == cardNumber) {
+    cardFrontNumber.textContent = e.target.value;
+  }
+
+  !cardHolderName.value ? (cardFrontName.textContent = "JANE APPLESEED") : null;
+
+  !cardNumber.value
+    ? (cardFrontNumber.textContent = "0000 0000 0000 0000")
+    : null;
 });
